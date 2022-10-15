aef66513f88b4cd2d7be5dd4c6f8079ef6c2d28b
liuxueyang457 <liuxueyang457@163.com>
2022-10-16 02:55:54 +0800

    leetcode 面试题 03.02. 栈的最小值

---
+++ leetcode/min-stack-lcci.cpp
@@ -0,0 +1,119 @@
+// Date: Sun Oct 16 02:45:53 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+// 面试题 03.02. 栈的最小值
+// https://leetcode.cn/problems/min-stack-lcci/description/
+class MinStack {
+public:
+  stack<int> st1, st2;
+
+  /** initialize your data structure here. */
+  MinStack() {
+    st1 = {};
+    st2 = {};
+  }
+
+  void push(int x) {
+    st1.push(x);
+    if (st2.empty()) {
+      st2.push(x);
+    } else if (nonempty(st2) && x <= st2.top()) {
+      st2.push(x);
+    }
+  }
+
+  void pop() {
+    if (st1.empty()) return;
+    int x = st1.top();
+    st1.pop();
+    if (nonempty(st2) && x == st2.top()) st2.pop();
+  }
+
+  int top() {
+    if (st1.empty()) return -1;
+    return st1.top();
+  }
+
+  int getMin() {
+    if (st2.empty()) return -1;
+    return st2.top();
+  }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
