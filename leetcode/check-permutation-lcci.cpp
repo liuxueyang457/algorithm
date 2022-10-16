94d5babd5f00f04de037b5c49bbe8bc1846ba5e4
liuxueyang457 <liuxueyang457@163.com>
2022-10-16 11:11:34 +0800

    leetcode 面试题 01.02. 判定是否互为字符重排

---
+++ leetcode/check-permutation-lcci.cpp
@@ -0,0 +1,94 @@
+// Date: Sun Oct 16 10:32:51 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+// 面试题 01.02. 判定是否互为字符重排
+// https://leetcode.cn/problems/check-permutation-lcci/
+
+class Solution {
+public:
+  bool CheckPermutation(string s1, string s2) {
+    int n = SZ(s1), m = SZ(s2);
+    if (n != m) return false;
+
+    map<char, int> m1, m2;
+    REP(i, 0, n) {
+      m1[s1[i]]++;
+      m2[s2[i]]++;
+    }
+
+    for (auto &p : m1) {
+      if (!has(m2, p.f1) || m2[p.f1] != p.f2) return false;
+    }
+
+    return true;
+  }
+};
