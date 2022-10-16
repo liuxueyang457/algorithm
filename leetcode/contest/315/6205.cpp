42e900ff36810c3fd724539b687e53f658d592b2
liuxueyang457 <liuxueyang457@163.com>
2022-10-16 13:00:47 +0800

    leetcode 6205. 反转之后不同整数的数目

---
+++ leetcode/contest/315/6205.cpp
@@ -0,0 +1,97 @@
+// Date: Sun Oct 16 12:52:08 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+// 6205. 反转之后不同整数的数目
+// https://leetcode.cn/problems/count-number-of-distinct-integers-after-reverse-operations/
+
+class Solution {
+public:
+  int get(int n) {
+    int res = 0;
+    while (n) {
+      res = res * 10 + (n % 10);
+      n /= 10;
+    }
+    return res;
+  }
+
+  int countDistinctIntegers(vector<int>& a) {
+    set<int> s;
+    int n = SZ(a);
+    REP(i, 0, n) {
+      s.insert(a[i]);
+      s.insert(get(a[i]));
+    }
+
+    return SZ(s);
+  }
+};
