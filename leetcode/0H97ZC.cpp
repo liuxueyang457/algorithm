e2ceddd645f3cb70557bfc4628a94754609125f7
liuxueyang457 <liuxueyang457@163.com>
2022-10-17 20:22:37 +0800

    leetcode 剑指 Offer II 075. 数组相对排序

---
+++ leetcode/0H97ZC.cpp
@@ -0,0 +1,103 @@
+// Date: Mon Oct 17 19:48:50 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+// 剑指 Offer II 075. 数组相对排序
+// https://leetcode.cn/problems/0H97ZC/
+
+class Solution {
+public:
+  vector<int> relativeSortArray(vector<int>& a, vector<int>& b) {
+    int n = SZ(a), m = SZ(b);
+    set<int> vis;
+    REP(i, 0, m) vis.insert(b[i]);
+
+    VI rem;
+    map<int, int> occ;
+    REP(i, 0, n) {
+      if (has(vis, a[i])) occ[a[i]]++;
+      else rem.pb(a[i]);
+    }
+
+    sort(all(rem));
+
+    VI res;
+    REP(i, 0, m) {
+      if (has(occ, b[i])) {
+        while (occ[b[i]]--) res.pb(b[i]);
+      }
+    }
+
+    REP(i, 0, SZ(rem)) res.pb(rem[i]);
+
+    return res;
+  }
+};
