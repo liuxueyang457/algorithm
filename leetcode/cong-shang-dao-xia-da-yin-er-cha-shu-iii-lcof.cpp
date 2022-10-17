2f14b877fc83d0c2ff22f25df919ed98b4d7dc11
liuxueyang457 <liuxueyang457@163.com>
2022-10-17 20:37:37 +0800

    leetcode 剑指 Offer 32 - III. 从上到下打印二叉树 III

---
+++ leetcode/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof.cpp
@@ -0,0 +1,123 @@
+// Date: Mon Oct 17 20:29:27 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+// 剑指 Offer 32 - III. 从上到下打印二叉树 III
+// https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  vector<vector<int>> levelOrder(TreeNode* ro) {
+    using PTI = pair<TNP, int>;
+    int level, cur = 0;
+    VI tmp;
+    vector<VI> res;
+    if (!ro) return res;
+    queue<PTI> q;
+    q.push({ro, cur});
+
+    while (nonempty(q)) {
+      auto t = q.front(); q.pop();
+      auto ver = t.f1;
+      level = t.f2;
+
+      if (level == cur) {
+        tmp.pb(ver->val);
+      } else {
+        if (cur & 1) reverse(all(tmp));
+
+        cur = level;
+        res.pb(tmp);
+        tmp = {ver->val};
+      }
+
+      auto l = ver->left, r = ver->right;
+      if (l) q.push({l, level + 1});
+      if (r) q.push({r, level + 1});
+    }
+
+    if (nonempty(tmp)) {
+      if (cur & 1) reverse(all(tmp));
+      res.pb(tmp);
+    }
+
+    return res;
+  }
+};
