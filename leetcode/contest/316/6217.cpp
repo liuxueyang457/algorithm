a91fb9a9d05a090d385f932fa8a498c881b044aa
liuxueyang457 <liuxueyang457@163.com>
2022-10-23 19:30:56 +0800

    leetcode 6217

---
+++ leetcode/contest/316/6217.cpp
@@ -0,0 +1,99 @@
+// Date: Sun Oct 23 19:26:01 2022
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const double eps = 1e-8;
+const int dir[8][2] = {
+  {0, 1}, {0, -1},
+  {1, 0}, {-1, 0},
+  {1, 1}, {1, -1},
+  {-1, 1}, {-1, -1},
+};
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+const ull Pr = 131;
+
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#define REP(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define PER(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define REP1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define PER1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define REPE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nonempty(a) (!a.empty())
+#define all(a) (a).begin(),(a).end()
+#define SZ(a) int((a).size())
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x,y) cout << #x" = " << x << " "#y" = " << y << endl;
+#define debug3(x,y,z) cout << #x" = " << x << " "#y" = " << y << " "#z" = " << z << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#endif
+
+#ifdef _DEBUG
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif
+
+class Solution {
+public:
+  long long makeSimilar(vector<int>& a, vector<int>& b) {
+    int n = SZ(a);
+    VI a1, a2, b1, b2;
+    REP(i, 0, n) {
+      if (a[i] & 1) a1.pb(a[i]);
+      else a2.pb(a[i]);
+
+      if (b[i] & 1) b1.pb(b[i]);
+      else b2.pb(b[i]);
+    }
+    sort(all(a1)); sort(all(a2));
+    sort(all(b1)); sort(all(b2));
+    ll res = 0;
+    REP(i, 0, SZ(a1)) {
+      res += abs(b1[i] - a1[i]) / 2;
+    }
+
+    REP(i, 0, SZ(a2)) {
+      res += abs(b2[i] - a2[i]) / 2;
+    }
+
+    res /= 2;
+    return res;
+  }
+};
